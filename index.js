1b3271721e0d5b93aafc2451ddaf1fb62cbf4d1e
Saurabh Sharma <59047214+Saurabhcodechef@users.noreply.github.com>
2022-05-06 23:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ function makeSound(key){
       audio1.play();
       break;
     case 'a':
-      let audio2 = new Audio('/sounds/tom-1.mp3');
+      let audio2 = new Audio('sounds/tom-1.mp3');
       audio2.play();
       break;
     case 's':
-      let audio3 = new Audio('/sounds/tom-2.mp3');
+      let audio3 = new Audio('sounds/tom-2.mp3');
       audio3.play();
       break;
     case 'd':
-      let audio4 = new Audio('/sounds/tom-4.mp3');
+      let audio4 = new Audio('sounds/tom-4.mp3');
       audio4.play();
       break;
     case 'j':
-      let audio5 = new Audio('/sounds/snare.mp3');
+      let audio5 = new Audio('sounds/snare.mp3');
       audio5.play();
       break;
     case 'k':
-      let audio6 = new Audio('/sounds/kick-bass.mp3');
+      let audio6 = new Audio('sounds/kick-bass.mp3');
       audio6.play();
       break;
     case 'l':
-      let audio7 = new Audio('/sounds/crash.mp3');
+      let audio7 = new Audio('sounds/crash.mp3');
       audio7.play();
       break;
     default:
